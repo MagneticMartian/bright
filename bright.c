9f563443c20b8e47f7a3a544a33151168dab630c
MagneticMartian <60756647+MagneticMartian@users.noreply.github.com>
2022-03-01 09:24:55 -0600

    Update bright.c

--- bright.c
+++ bright.c
@@ -42,9 +42,7 @@ main (int argc, char* argv[])
         {
             case 0:
                 if (long_options[option_index].flag != 0) break;
-
                 printf("option %s", long_options[option_index].name);
-
                 if (optarg) printf(" with arg %s", optarg);
                 printf("\n");
                 break;
